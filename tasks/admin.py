f9bfad8be7cb9f0f45fdee97315cf8b4a1b62f02
Thelastguru6 <thelastguru6@gmail.com>
2025-08-30 13:59:53 +0000

    Edited amins file

--- tasks/admin.py
+++ tasks/admin.py
@@ -6,3 +6,7 @@ class TaskAdmin(admin.ModelAdmin):
     list_display = ('user', 'title',  'completed', 'created_at')
     list_filter = ('completed', 'created_at')
     search_fields = ('title', 'description')
+
+admin.site.site_header = "TaskBuddy Admin"
+admin.site.site_title = "TaskBuddy Admin Portal"
+admin.site.index_title = "Welcome to TaskBuddy Admin Portal"
